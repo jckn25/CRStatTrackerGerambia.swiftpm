4a3518a69ca9c24a79e6290aa237dc696aa38037
Jackson <jacksongerambia@yahoo.com>
2025-11-17 08:48:14 -0600

    commit start of sprint 2

---
+++ AnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CRStatTrackerGerambia
+//
+//  Created by JACKSON GERAMBIA on 11/17/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
